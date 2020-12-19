8ce35ce8dc41fe1e2bf513431d229a358a4474a7
Omprakash Reddy <prakashbom84@gmail.com>
2020-12-19 20:36:42 +0530

    --- Auto-Git Commit ---

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
+    # image=models.ImageField(upload_to='myportfolio/images')
     description = models.TextField()
     date = models.DateField()
 
